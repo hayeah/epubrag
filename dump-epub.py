18c70a2a6f8aa5dd49fb65de3d285598f7087b5c
Howard Yeh <hayeah@gmail.com>
2024-02-20 15:34:58 +0700

    text blocks iterator

--- dump-epub.py
+++ dump-epub.py
@@ -2,7 +2,7 @@ import os
 import glob
 from lxml import etree
 
-from typing import Optional
+from typing import Iterator, Optional
 from bs4 import BeautifulSoup
 from functools import cached_property, cache
 from urllib.parse import urlparse
@@ -14,10 +14,68 @@ class Chapter(NamedTuple):
     """
     A chapter in an EPUB.
     """
+
+    # chapter index within a book
+    index: int 
     href: str
     text: str
     path: str
 
+class TextBlock(NamedTuple):
+    """
+    A block of text in an EPUB chapter.
+    """
+    # text block index within a chapter
+    chapter: Chapter
+
+    fm: bool
+
+    index: int
+    page: int
+    dom: BeautifulSoup
+    text: str
+    
+
+from enum import IntEnum
+
+class PageType(IntEnum):
+    FRONTMATTER = 1
+    BODY = 2
+    ERROR = 3
+
+
+def roman_to_int(s: str) -> int:
+    roman_map: dict[str, int] = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    total: int = 0
+    prev_value: int = 0
+    
+    for char in reversed(s):
+        if char not in roman_map:
+            raise ValueError("Invalid Roman numeral")
+        value: int = roman_map[char]
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+        prev_value = value
+    
+    return total
+
+def parse_pagenumber(input_str: str) -> tuple[int, PageType]:
+    # Try parsing as an integer
+    try:
+        return int(input_str), PageType.BODY
+    except ValueError:
+        pass  # Not an integer, try parsing as a Roman numeral
+
+    # Try parsing as a Roman numeral
+    try:
+        return roman_to_int(input_str), PageType.FRONTMATTER
+    except ValueError:
+        pass  # Not a valid Roman numeral
+
+    # If neither parsing succeeded, return 0
+    return 0, PageType.ERROR
 
 # # \xa0 is actually non-breaking space in Latin1 (ISO 8859-1), also chr(160). You should replace it with a space.    
     
@@ -51,7 +109,8 @@ class ChapterScraper():
         """
 
         # direct children (p, div) of body
-        return self.dom.select('body > p, body > div')
+        # return self.dom.select('body > p, body > div')
+        return self.dom.select('body > *')
     
 
 class EPUBScraper:
@@ -108,16 +167,53 @@ class EPUBScraper:
 
         # extract href and text
         chapters = []
+        i = 0
         for link in links:
             href = link['href']
             text = link.text.strip()
-            chapters.append(Chapter(href, text, os.path.join(relative_root, href_pathonly(href))))
+            chapters.append(Chapter(i, href, text, os.path.join(relative_root, href_pathonly(href))))
+            i += 1
 
         return chapters
     
     # @cached
     def chapter_scraper(self, i) -> ChapterScraper:
         return ChapterScraper(self.chapters[i])
+    
+    # returns an iterator of TextBlock
+    def text_blocks(self) -> Iterator[TextBlock]:
+        page: int = int(0)
+        fm: bool = False
+
+        for chapter in self.chapters:
+            scraper = ChapterScraper(chapter)
+            for i, block in enumerate(scraper.blocks()):
+                # try to find page number by parsing '<a id="page55">'
+                pagetag = block.find('a', id=lambda x: x and x.startswith('page'))
+                if pagetag:
+                    pagestr = pagetag['id'][4:].upper()
+                    nextpage, pagetype = parse_pagenumber(pagestr)
+                    if pagetype == PageType.BODY:
+                        page = nextpage
+                        fm = False
+                    elif pagetype == PageType.FRONTMATTER:
+                        page = nextpage
+                        fm = True
+
+                # not sure if there is a better way to remove <br> tags...
+                for br in block.find_all('br'):
+                    br.replace_with('\n')
+                
+                text = block.get_text(separator='').strip()
+
+                # text = block.get_text(separator=' ')
+                if len(text) == 0:
+                    continue
+
+                yield TextBlock(chapter=chapter, fm=fm, index=i, dom=block, page=page, text=text)
+
+
+
 
 from zipfile import ZipFile
 def extract_epub(epub_path: str, output_dir: Optional[str] = None) -> None:
@@ -141,34 +237,18 @@ def extract_epub(epub_path: str, output_dir: Optional[str] = None) -> None:
 
 
 
-extract_epub('Master Of The Senate (Robert A. Caro) (Z-Library).epub')
+# extract_epub('Master Of The Senate (Robert A. Caro) (Z-Library).epub')
 epub = EPUBScraper('Master Of The Senate (Robert A. Caro) (Z-Library)')
 # epub = EPUBScraper('Means Of Ascent (Robert A. Caro) (Z-Library)')
 
 # print(epub.opf_dom)
-print(epub.nav_path)
-print(epub.nav_dom)
+# print(epub.nav_path)
+# print(epub.nav_dom)
+# pprint(epub.chapters)
 
-
-pprint(epub.chapters)
-
-ch1 = epub.chapter_scraper(0)
-
-for block in ch1.blocks():
-    # why are there wierd link breaks?
-    # text = block.text.strip()
-    # block.contents
-    # print(list(block.strings))
-    # print(block.encode(indent_level=None))
-    # print(block.encode(formatter=None, indent_level=None))
-    # print(block.contents)
-    # for c in block.contents:
-    #     print(type(c))
-    #     print(str(c))
-        # print(list(c.stripped_strings))
-    print(block.get_text(separator=''))
-    # content = block.get_text(separator=' ', strip=True)
-    # print(content)
-    # print()
+for block in epub.text_blocks():
+    # pprint(block)
+    print(block.text)
     print('-' * 80)
-
+    if block.chapter.index > 2:
+        break
